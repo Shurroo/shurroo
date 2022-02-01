1699ee155c3544ef936bde2f8f3755bba7c00e36
IanTaylorFB <ian.taylor@flyingbinary.com>
2022-02-01 10:21:33 +0000

    FIX: Include manual removal of Homebrew folders for Intel Macs only
      - Only handling the Intel Mac case at present
      - Apple Silicon Macs have the folders in a different location
      - We are not yet handling that use case

--- 502-uninstall-homebrew.sh
+++ 502-uninstall-homebrew.sh
@@ -6,3 +6,11 @@
 
 printf "Uninstalling Homebrew\n"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh)"
+
+# These folders apply to Intel Macs only
+# We are not yet handling Apple Silicon Macs
+printf "Removing Homebrew folders - your password may be necessary\n"
+sudo rm -rf /usr/local/Homebrew/
+sudo rm -rf /usr/local/etc/
+sudo rm -rf /usr/local/share/
+sudo rm -rf /usr/local/var/
