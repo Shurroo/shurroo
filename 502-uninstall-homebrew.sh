36b46ba277168a6b8ecb9cce154bd9847d25d916
IanTaylorFB <ian.taylor@flyingbinary.com>
2022-02-02 08:34:29 +0000

    FIX: Run brew uninstall with sudo, include other folders in manual removal

--- 502-uninstall-homebrew.sh
+++ 502-uninstall-homebrew.sh
@@ -5,12 +5,20 @@
 #   - 12.x Monterey
 
 printf "Uninstalling Homebrew\n"
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh)"
+sudo /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh)"
 
 # These folders apply to Intel Macs only
 # We are not yet handling Apple Silicon Macs
 printf "Removing Homebrew folders - your password may be necessary\n"
-sudo rm -rf /usr/local/Homebrew/
+sudo rm -rf /usr/local/bin/
+sudo rm -rf /usr/local/Caskroom/
+sudo rm -rf /usr/local/Cellar/
 sudo rm -rf /usr/local/etc/
+sudo rm -rf /usr/local/Frameworks/
+sudo rm -rf /usr/local/Homebrew/
+sudo rm -rf /usr/local/include/
+sudo rm -rf /usr/local/lib/
+sudo rm -rf /usr/local/opt/
+sudo rm -rf /usr/local/sbin/
 sudo rm -rf /usr/local/share/
 sudo rm -rf /usr/local/var/
