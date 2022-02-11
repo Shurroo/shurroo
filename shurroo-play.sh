978db18f73b2ef925ab3a7853b919534e7da3931
IanTaylorFB <ian.taylor@flyingbinary.com>
2022-02-11 09:10:17 +0000

    Allow options for ansible and notify playbook selection
      - Allow options before playbook name
      - Especially '-v' '-vv' for debugging
      - Split these and pass to ansible
      - Show the selected custom or default playbook location

--- shurroo-play.sh
+++ shurroo-play.sh
@@ -4,22 +4,35 @@ USER_HOME=$(printf "%s" ~)
 CUSTOM_LOCATION="/Volumes/Shurroo"
 DEFAULT_LOCATION="${USER_HOME}""/.shurroo"
 
+declare -a ANSIBLE_OPTS
+for arg in "${@}"
+do
+  if [[ "$arg" =~ \-.* ]]
+  then
+    ANSIBLE_OPTS+="$arg"
+    shift
+  fi
+done
+
 function run_playbook() {
   CUSTOM_FILE="${CUSTOM_LOCATION}""/${1}/${1}.yml"
   DEFAULT_FILE="${DEFAULT_LOCATION}""/shurroo/${1}.yml"
 
   if [[ -f "${CUSTOM_FILE}" ]]
   then
+    FILE_SOURCE="custom"
     PLAYBOOK_FILE="${CUSTOM_FILE}"
   elif [[ -f "${DEFAULT_FILE}" ]]
   then
+    FILE_SOURCE="default"
     PLAYBOOK_FILE="${DEFAULT_FILE}"
   else
     printf "Missing action playbook for '%s'\n" "${1}"
     exit 1
   fi
   cd "${DEFAULT_LOCATION}" >/dev/null
-  ansible-playbook -i "localhost," "${PLAYBOOK_FILE}"
+  printf "Using %s playbook file at %s\n" "$FILE_SOURCE" "$PLAYBOOK_FILE"
+  ansible-playbook -i "localhost," "${ANSIBLE_OPTS[@]}"  "${PLAYBOOK_FILE}"
 }
 
 for role in "${@}"
